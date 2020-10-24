1e92792cf4eeb1ec05bf9820f4898e993d2b4ab5
SaliniRamkiran <SaliniRamkiran@LAPTOP-4D2TC3QU>
2020-10-24 20:30:41 +0530

    Basic pom updates

---
+++ basiccrud/src/main/java/com/ramproject/basiccrud/BasicCrudApp.java
@@ -0,0 +1,17 @@
+package com.ramproject.basiccrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BasicCrudApp 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BasicCrudApp.class, args);
+    }
+}
